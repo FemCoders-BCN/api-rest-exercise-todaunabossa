031d72870b9b114d3e87c99470407e34e5270df3
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-09 19:53:50 +0200

    correcciones

--- src/services/FavoriteService.js
+++ src/services/FavoriteService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.baseLocalURL = 'http://localhost:5000/';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
 
 export const FavoriteService = {
   urlGetFavorites: '/localhost:5000/',
