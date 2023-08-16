fba961f005eaa96ee8cc181cd8d6e094d349e1fc
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-08-16 12:09:01 +0200

    separacion de logica y componente primera llamada

--- src/components/pictureObject/PictureObject.js
+++ src/components/pictureObject/PictureObject.js
@@ -1,64 +1,18 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import { LoremPicsumService } from '../../services/LoremPicsumService';
-import './pictureObject.css';
+import React from 'react';
 
-function PictureObject() {
-  const [pictures, setPictures] = useState(['']);
-  const [favorites, setFavorites] = useState(['']);
-
-  useEffect(() => {
-    const service = LoremPicsumService;
-
-    service.getAll()
-      .then(response => setPictures(response.data))
-      .catch(error => console.log(error));
-  }, []);
-
-  const toggleFavorite = (picture) => {
-    if (favorites.some(favorite => favorite.id === picture.id)) {
-      removeFromFavorites(picture.id);
-    } else {
-      addToFavorites(picture);
-    }
-  };
-
-  const addToFavorites = (picture) => {
-    setFavorites(prevFavorites => [...prevFavorites, picture]);
-
-    axios.post('http://localhost:5000/pictures', picture)
-      .then(response => {
-        console.log('Elemento guardado como favorito:', response.data);
-      })
-      .catch(error => {
-        console.log('Error al guardar el elemento como favorito:', error);
-      });
-  };
-
-  const removeFromFavorites = (pictureId) => {
-    setFavorites(prevFavorites => prevFavorites.filter(picture => picture.id !== pictureId));
-     axios.delete(`http://localhost:5000/pictures/${pictureId}`)
-      .then(response => {console.log('Elemento quitado de favoritos:', response.data);
-      }).catch(error => {console.log('Error al quitar el elemento de favoritos:', error);
-      });
-  };
-
+function PictureObject({ picture, isFavorite, toggleFavorite }) {
   return (
     <div>
       <div className="object-container">
-        {pictures.map(picture => (
-          <div key={picture.id}>
-            <img id='object-picture' src={picture.download_url} alt="" />
-            <p>ID: {picture.id}</p>
-            <p>Autor: {picture.author}</p>
+        <img id='object-picture' src={picture.download_url} alt="" />
+        <p>ID: {picture.id}</p>
+        <p>Autor: {picture.author}</p>
 
-            {favorites.some(favorite => favorite.id === picture.id) ? (
-              <button className='add-Favorites' onClick={() => toggleFavorite(picture)}>Quitar de favoritos</button>
-            ) : (
-              <button className='add-Favorites' onClick={() => toggleFavorite(picture)}>Guardar como favorito</button>
-            )}
-          </div>
-        ))}
+        {isFavorite ? (
+          <button className='add-Favorites' onClick={() => toggleFavorite(picture)}>Quitar de favoritos</button>
+        ) : (
+          <button className='add-Favorites' onClick={() => toggleFavorite(picture)}>Guardar como favorito</button>
+        )}
       </div>
     </div>
   );
--- src/pages/picturesPage/PicturesPage.js
+++ src/pages/picturesPage/PicturesPage.js
@@ -1,15 +1,75 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import PictureObject from '../../components/pictureObject/PictureObject'
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import PictureObject from '../../components/pictureObject/PictureObject';
+import axios from 'axios';
+import { LoremPicsumService } from '../../services/LoremPicsumService';
+import '../../components/pictureObject/pictureObject.css'
 
-const PicturesPage = () => {
+function PicturesPage() {
+  const [pictures, setPictures] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const fetchPictures = async () => {
+      try {
+        const response = await axios.get(LoremPicsumService.urlGetAll); // Actualiza la URL
+        setPictures(response.data);
+      } catch (error) {
+        console.log('Error al obtener las imágenes:', error);
+      }
+    };
+
+    fetchPictures();
+  }, []);
+
+  const toggleFavorite = (picture) => {
+    if (favorites.some(favorite => favorite.id === picture.id)) {
+      removeFromFavorites(picture.id);
+    } else {
+      addToFavorites(picture);
+    }
+  };
+
+  const addToFavorites = (picture) => {
+    setFavorites(prevFavorites => [...prevFavorites, picture]);
+
+    axios.post('http://localhost:5000/pictures', picture)
+      .then(response => {
+        console.log('Elemento guardado como favorito:', response.data);
+      })
+      .catch(error => {
+        console.log('Error al guardar el elemento como favorito:', error);
+      });
+  };
+
+  const removeFromFavorites = (pictureId) => {
+    setFavorites(prevFavorites => prevFavorites.filter(picture => picture.id !== pictureId));
+
+    axios.delete(`http://localhost:5000/pictures/${pictureId}`)
+      .then(response => {
+        console.log('Elemento quitado de favoritos:', response.data);
+      })
+      .catch(error => {
+        console.log('Error al quitar el elemento de favoritos:', error);
+      });
+  };
+
   return (
     <main>
       <h2>Aquí estarán las 30 objetos de la primera llamada</h2>
       <Navbar />
-      <PictureObject />
+      <div className="pictures-container">
+        {pictures.map(picture => (
+          <PictureObject
+            key={picture.id}
+            picture={picture}
+            isFavorite={favorites.some(favorite => favorite.id === picture.id)}
+            toggleFavorite={toggleFavorite}
+          />
+        ))}
+      </div>
     </main>
   );
-};
+}
 
 export default PicturesPage;
